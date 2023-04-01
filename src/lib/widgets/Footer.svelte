9f375bb3e9a9279251cb520835031894de9023e9
Szalovszky Dávid <david@szalovszky.com>
2023-04-01 16:23:59 +0000

    initial code commit

---
+++ src/lib/widgets/Footer.svelte
@@ -0,0 +1,8 @@
+<div>
+  <a class="underline" href="https://github.com/szalovszky/me">source code</a>
+  <p class="text-gray-500">Szalovszky Dávid</p>
+</div>
+
+<style lang="scss">
+  @import "../Colors.scss";
+</style>
---
+++ src/lib/widgets/home/ContactEntry.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let key: string;
+  export let link: string;
+  export let value: string;
+</script>
+
+<tr>
+  <td class="text-gray-400">{key}</td>
+  <td class="pl-2"><a class="underline" href={link}>{value}</a></td>
+</tr>
